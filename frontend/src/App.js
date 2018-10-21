d216080cc15c76021f5e92b2bb8cf351fb537d79
jeminix <jeminix04@gmail.com>
2018-10-21 08:16:02 +0000

    reinstall Create-React-App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import styles from './App.scss';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <header className={styles.App__header}>
-          <img src={logo} className={styles.App__logo} alt="logo" />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className={styles.App__link}
+            className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
