c203f0792bfc7fb06462c1884e0b41a083c84d17
jeminix <jeminix04@gmail.com>
2018-10-21 16:56:57 +0000

    Customizing the Django Admin

--- aweach/images/models.py
+++ aweach/images/models.py
@@ -3,6 +3,7 @@ from aweach.users import models as user_models
 
 # Create your models here.
 
+
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,10 +16,14 @@ class TimeStampedModel(models.Model):
 class Image(TimeStampedModel):
 
     """ Image Model """
-    
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
 
 
 class Comment(TimeStampedModel):
@@ -29,6 +34,9 @@ class Comment(TimeStampedModel):
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT)
     image = models.ForeignKey(Image, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.message
+
 
 class Like(TimeStampedModel):
 
@@ -36,3 +44,6 @@ class Like(TimeStampedModel):
 
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT)
     image = models.ForeignKey(Image, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '{} {}'.format(self.creator.username, self.image.caption)
